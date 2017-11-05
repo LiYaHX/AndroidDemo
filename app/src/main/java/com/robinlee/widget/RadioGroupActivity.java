6eaf61abd8524eccebd9a1846815f2357303bf5b
LiYaHX <liya.bishe@gmail.com>
2017-11-05 18:33:32 +0800

    custom radio button widget

--- app/src/main/java/com/robinlee/widget/RadioGroupActivity.java
+++ app/src/main/java/com/robinlee/widget/RadioGroupActivity.java
@@ -1,15 +1,42 @@
 package com.robinlee.widget;
 
+import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.CompoundButton;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 
 import com.robinlee.androiddemo.R;
 
 public class RadioGroupActivity extends AppCompatActivity {
 
+    private RadioGroup radioGroup;
+    private RadioButton mRadioBtnAsc;
+    private RadioButton mRadioBtnDesc;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_radio_group);
+        initWidget();
+    }
+
+    private void initWidget() {
+        radioGroup = (RadioGroup) findViewById(R.id.mRadioGroup);
+        mRadioBtnAsc = (RadioButton) findViewById(R.id.mRadioBtnAsc);
+        mRadioBtnAsc.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                buttonView.setTypeface(isChecked ? Typeface.DEFAULT_BOLD : Typeface.DEFAULT);
+            }
+        });
+        mRadioBtnDesc = (RadioButton) findViewById(R.id.mRadioBtnDesc);
+        mRadioBtnDesc.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                buttonView.setTypeface(isChecked ? Typeface.DEFAULT_BOLD : Typeface.DEFAULT);
+            }
+        });
     }
 }
