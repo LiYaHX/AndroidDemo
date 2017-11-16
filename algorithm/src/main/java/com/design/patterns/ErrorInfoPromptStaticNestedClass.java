b759b2a91da770bdd8d076e348a07d88583c4b7a
LiYaHX <liya.bishe@gmail.com>
2017-11-16 15:33:29 +0800

    nothing

--- algorithm/src/main/java/com/design/patterns/ErrorInfoPromptStaticNestedClass.java
+++ algorithm/src/main/java/com/design/patterns/ErrorInfoPromptStaticNestedClass.java
@@ -4,8 +4,8 @@ package com.design.patterns;
  * Created by robinlee on 2017/11/16.
  *
  *  6. 静态内部类 (static nested class)
- *     优点：《Effective Java》推荐
- *     原理描述：
+ *     优点 ：《Effective Java》推荐
+ *     原理描述 ：
  *            （1）使用JVM本身机制保证了线程安全问题；
  *            （2）ErrorInfoPromptStaticNestedClassHolder是私有的 ，除了 getInstance() 没有办法访问它，因此它是懒汉式的；
  *            （3）读取实例是不会进行同步，没有性能缺陷；
